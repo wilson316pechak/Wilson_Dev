11071c486076ee5a133334bec53838c1918defd6
wilson316pechak <ss@gmail.com>
2021-03-15 18:40:16 +0530

    Trial

--- appdemo/src/main/org.cap.demo/appdemo/MainClass.java
+++ appdemo/src/main/org.cap.demo/appdemo/MainClass.java
@@ -1,11 +1,15 @@
 package appdemo;
 
+import java.awt.Color;
+
 public class MainClass 
 {
 	public static void main(String[] args) 
 	 {
 		Demo demo = new Demo();
 		demo.stage();
+		Color color = new Color(0);
+		
 	}
 	}
 
